512889db00c8c14fc59b90081b2315956d7572ce
Ifeoluwah <spencerife.21@gmail.com>
2026-02-18 20:58:13 +0100

    feat(ui): add another section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/Features";
 import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -76,6 +77,14 @@ export default function Home() {
             </div>
           </article>
         </div>
+      </section>
+      <section className="bg-theme-300 flex w-full flex-col items-center gap-8 px-5 py-10 md:py-20 xl:py-30">
+        <h3 className="font-radio-canada-big text-center text-2xl leading-[100%] font-medium -tracking-[0.72px] md:text-[32px] md:-tracking-[0.96px] xl:text-[40px] xl:-tracking-[1.2px]">
+          Ready to operationalize your sustainability goals?
+        </h3>
+        <Button className="h-auto rounded-none p-4 text-[14px] font-medium">
+          Request a demo
+        </Button>
       </section>
     </main>
   );
