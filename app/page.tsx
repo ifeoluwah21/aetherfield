1da8f5ae7d8a5ac818aa0d5c26193d9bacddc3ec
Ifeoluwah <spencerife.21@gmail.com>
2026-02-19 22:11:21 +0100

    feat(ui): add more section

--- app/page.tsx
+++ app/page.tsx
@@ -78,6 +78,107 @@ export default function Home() {
           </article>
         </div>
       </section>
+      <section className="mx-auto max-w-245 px-5 py-10 md:py-20">
+        <article className="bg-theme-300 flex flex-col gap-6 rounded-3xl p-5 md:flex-row md:gap-10">
+          <Image
+            src={"/Image-1.svg"}
+            alt="Group of colleagues greeting each other in a stylized blue duotone treatment"
+            width={498}
+            height={280}
+            className="w-full"
+          />
+          <div className="flex flex-col gap-6 md:justify-center">
+            <div className="flex flex-col gap-3 md:gap-4">
+              <h4 className="font-radio-canada-big text-[18px] font-medium -tracking-[0.36px] md:text-[20px] md:-tracking-[0.4px]">
+                Why Acme Inc chose Aetherfield
+              </h4>
+              <p className="font-source-serif-pro text-[18px] leading-[120%] font-normal -tracking-[0.54px] md:text-[20px] md:-tracking-[0.8px]">
+                With fragmented data and growing reporting pressure, Acme turned
+                to Aetherfield to streamline their ESG workflows. The result?
+                Faster decisions, fewer spreadsheets, and 34% more coverage.
+              </p>
+            </div>
+            <Button className="h-auto rounded-none p-3 md:w-max">
+              Read case study
+            </Button>
+          </div>
+        </article>
+      </section>
+      <section className="relative flex w-full flex-col gap-6 px-5 md:gap-10">
+        <h3 className="font-radio-canada-big text-center text-2xl -tracking-[0.72px] md:text-[32px] md:-tracking-[0.96px] xl:text-[40px] xl:-tracking-[1.2px]">
+          From the journal
+        </h3>
+        {/* <Image
+          src={"/Sticker 2.svg"}
+          alt=""
+          height={154}
+          width={400}
+          className="absolute -top-27.25 -left-71.75"
+        /> */}
+        <div className="divide-theme-200 border-theme-200 mx-auto flex max-w-155 flex-col gap-6 divide-y border-t">
+          <article className="flex flex-col gap-4 py-6 md:flex-row">
+            <Image
+              src={"/journal-1.svg"}
+              width={335}
+              height={203}
+              alt="Sheer white fabric billowing gently in a bright blue sky"
+              className="aspect-[1.65] w-full object-cover md:w-41.25"
+            />
+            <div className="flex grow flex-col gap-2">
+              <h4 className="font-radio-canada-big text-[18px] font-medium -tracking-[0.36px] md:text-[20px] md:-tracking-[0.4px]">
+                How to Build a Climate-Ready Data Stack
+              </h4>
+              <p className="text-theme-100 flex items-center gap-2 font-mono text-sm font-normal">
+                <span>Insights </span>
+                <span>. </span>
+                <span>4 mins</span>
+              </p>
+            </div>
+          </article>
+          <article className="flex flex-col gap-4 py-6 md:flex-row">
+            <Image
+              src={"/journal-2.svg"}
+              width={335}
+              height={203}
+              alt="Sheer white fabric billowing gently in a bright blue sky"
+              className="aspect-[1.65] w-full object-cover md:w-41.25"
+            />
+            <div className="flex grow flex-col gap-2">
+              <h4 className="font-radio-canada-big text-[18px] font-medium -tracking-[0.36px] md:text-[20px] md:-tracking-[0.4px]">
+                Sustainability Isn&apos;t a Side Project: Making Impact
+                Operational
+              </h4>
+              <p className="text-theme-100 flex items-center gap-2 font-mono text-sm font-normal">
+                <span>Strategy </span>
+                <span>. </span>
+                <span>7 mins</span>
+              </p>
+            </div>
+          </article>
+          <article className="flex flex-col gap-4 py-6 md:flex-row">
+            <Image
+              src={"/journal-3.svg"}
+              width={335}
+              height={203}
+              alt="Sheer white fabric billowing gently in a bright blue sky"
+              className="aspect-[1.65] w-full object-cover md:w-41.25"
+            />
+            <div className="flex grow flex-col gap-2">
+              <h4 className="font-radio-canada-big text-[18px] font-medium -tracking-[0.36px] md:text-[20px] md:-tracking-[0.4px]">
+                Inside the Aetherfield Model: How We Turn Data Into Action
+              </h4>
+              <p className="text-theme-100 flex items-center gap-2 font-mono text-sm font-normal">
+                <span>Insights </span>
+                <span>. </span>
+                <span>5 mins</span>
+              </p>
+            </div>
+          </article>
+          <Button className="h-auto rounded-none p-3 md:mx-auto md:w-max">
+            View all articles
+          </Button>
+        </div>
+      </section>
       <section className="bg-theme-300 flex w-full flex-col items-center gap-8 px-5 py-10 md:py-20 xl:py-30">
         <h3 className="font-radio-canada-big text-center text-2xl leading-[100%] font-medium -tracking-[0.72px] md:text-[32px] md:-tracking-[0.96px] xl:text-[40px] xl:-tracking-[1.2px]">
           Ready to operationalize your sustainability goals?
