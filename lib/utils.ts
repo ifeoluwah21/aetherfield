57a1ff34406b768b4ac6858459ab4d3d6c8011b9
Ifeoluwah <spencerife.21@gmail.com>
2026-03-17 15:38:07 +0100

    fix: resolve build issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
