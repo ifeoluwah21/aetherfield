8a23d846cd08bf0ced1f50db642bc1427bf55784
Ifeoluwah <spencerife.21@gmail.com>
2026-03-17 16:25:06 +0100

    fix: API build issue

--- app/api/careers/route.ts
+++ app/api/careers/route.ts
@@ -1,6 +1,8 @@
 import { jobRoles } from "@/dal/careers";
 import { type NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const limit = searchParams.get("limit") || 5;
