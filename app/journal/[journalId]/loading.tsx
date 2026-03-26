cbf073b469ea044612a0bfd8cc082e3a3954737f
Ifeoluwah <spencerife.21@gmail.com>
2026-03-26 11:50:48 +0100

    feat(ui): add dummy loading page

---
+++ app/journal/[journalId]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loadingPage = () => {
+  return (
+    <div className="flex min-h-dvh w-full grow flex-col items-center justify-between bg-white">
+      Loading...
+    </div>
+  );
+};
+
+export default loadingPage;
